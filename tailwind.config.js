e8a4a9224acfa2db3fab6a9da594faaadefb639d
JhoannS <sebastianzamudio2004@gmail.com>
2025-01-25 12:16:39 -0500

    Conexion a base de datos 12:15, Ene 25 2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,44 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                mono: {
+                  blanco: '#F5EFFF',
+                  negro: '#060809',
+                },
+                bg: {
+                  empty: '#1F1D2B',
+                },
+                semaforo: {
+                  verde: '#1BCC75',
+                  amarillo: '#FFA823',
+                  rojo: '#FF3131',
+                },
+                secundary: {
+                  DEFAULT: '#1A2130',
+                  light: '#A5B8D4',
+                },
+                machine: {
+                  primary: '#56Ab98',
+                  secundary: '#87CBB9',
+                },
+                guru: {
+                  primary: '#C58940',
+                  secundary: '#E4CDA4',
+                },
+                shopper: {
+                  primary: '#3D4AEE',
+                  secundary: '#87A2FF',
+                },
+                essentials: {
+                  primary: '#FF3131',
+                  secundary: '#FF9D9D',
+                },
+                smart: {
+                  primary: '#9A48D5',
+                  secundary: '#DBBCF6',
+                },
+              },
         },
     },
     plugins: [],
